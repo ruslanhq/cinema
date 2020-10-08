d5715c8bb5fc338ffce5dc6c55747c2cb3e67e4f
ruslanhq <h.ruslan97@gmail.com>
2020-10-08 19:21:42 +0600

    Реализованны представления MovieList и MovieDetail, добавлены шаблоны и url

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -3,6 +3,7 @@ from datetime import date
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -65,6 +66,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('movie_detail', kwargs={'slug': self.url})
 
     class Meta:
         verbose_name = "Фильм"
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
+from .models import Movie
+
+
+class MoviesView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'movies/movie_list.html'
+
+
+class MovieDetail(DetailView):
+    model = Movie
+    slug_field = 'url'
+    template_name = 'movies/movie_detail.html'
